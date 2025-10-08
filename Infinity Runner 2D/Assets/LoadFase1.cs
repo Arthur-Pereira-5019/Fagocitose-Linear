0d5787326c3cd57bbe568bc446089aed6b844bf2
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-10-08 18:02:58 -0300

    Adicionado loader no Menu Principal

---
+++ Infinity Runner 2D/Assets/LoadFase1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+
+public class LoadFase1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void loadFase() {
+                SceneManager.LoadScene("Fase1"); 
+
+    }
+}
